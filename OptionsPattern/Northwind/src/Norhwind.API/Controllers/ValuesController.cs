ea4d785ea785bd19c6692741b3236bcabdee6a79
enesucar <50610802+enesucar@users.noreply.github.com>
2024-06-10 20:00:34 +0300

    Add files via upload

---
+++ OptionsPattern/Northwind/src/Norhwind.API/Controllers/ValuesController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+
+namespace Norhwind.API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ValuesController : ControllerBase
+{
+    private readonly DatabaseOptions _databaseOptions;
+
+    public ValuesController(IOptions<DatabaseOptions> databaseOptions)
+    {
+        _databaseOptions = databaseOptions.Value;
+    }
+
+    [HttpGet]
+    public IActionResult GetDatabaseOptions()
+    {
+        return Ok(_databaseOptions);
+    }
+}
---
+++ OptionsPattern/Northwind/src/Norhwind.API/DatabaseOptions.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Norhwind.API;
+
+public class DatabaseOptions
+{
+    [Required]
+    public string Url { get; set; } = null!;
+
+    [Required]
+    public string User { get; set; } = null!;
+
+    [Required]
+    public string Password { get; set; } = null!;
+}
